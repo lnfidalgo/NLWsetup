ca62ea6c5e1bc39822cfca9573679a9fc3ee1c8c
lnfidalgo <lucasfidalgo07@gmail.com>
2023-01-20 21:28:20 -0300

    front-end base

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -1,9 +1,11 @@
 interface HabitProps {
-  completed: number
+  completed: number;
 }
 
 export function Habit(props: HabitProps) {
   return (
-    <div className="bg-slate-500 w-10 h-10 text-white rounded m-2 flex items-center justify-center">{props.completed}</div>
-  )
+    <div className="bg-slate-500 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+      {props.completed}
+    </div>
+  );
 }
